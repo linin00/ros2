49c51dca576007c35fc477be821787896864629e
linin00 <lianjie35@outlook.com>
2022-11-24 08:22:34 +0000

    fix: camera canot compile

--- ros2/src/camera/src/listener.cpp
+++ ros2/src/camera/src/listener.cpp
@@ -1,7 +1,7 @@
 /*** 
  * @Author: linin00
  * @Date: 2022-11-24 07:19:10
- * @LastEditTime: 2022-11-24 07:33:01
+ * @LastEditTime: 2022-11-24 07:44:03
  * @LastEditors: linin00
  * @Description: 
  * @FilePath: /ros2/ros2/src/camera/src/listener.cpp
@@ -9,8 +9,8 @@
  */
 #include "realsense2_camera_msgs/msg/metadata.hpp"
 #include "rclcpp/rclcpp.hpp"
-static void handler(realsense2_camera_msgs::msg::Metadata msg) {
-  printf("[YDLIDAR INFO]: I get a msg: %s\n", msg.json_data);
+static void handler(realsense2_camera_msgs::msg::Metadata::SharedPtr msg) {
+  printf("[YDLIDAR INFO]: I get a msg: %s\n", msg->json_data.c_str());
 }
 int main(int argc, char ** argv) {
   rclcpp::init(argc, argv);
