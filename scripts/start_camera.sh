a37946b9fa07335d6e325d03d7cba56f82ab4c3f
linin00 <lianjie35@outlook.com>
2022-12-29 08:12:54 +0000

    巡线脚本和摄像头参数配置文件

--- scripts/start_camera.sh
+++ scripts/start_camera.sh
@@ -1,6 +1,8 @@
 # set -e
 # set -x
 realsense_path="/home/pi/codefield/realsense-ros"
+config_path="/home/pi/codefield/ros2/config/d435.yaml"
 cd ${realsense_path}
 source ./install/setup.bash
-ros2 launch realsense2_camera rs_launch.py rgb_camera.profile:=640x240x30
+# ros2 launch realsense2_camera rs_launch.py rgb_camera.profile:=640x240x30
+ros2 launch realsense2_camera rs_launch.py config_file:="'${config_path}'"
